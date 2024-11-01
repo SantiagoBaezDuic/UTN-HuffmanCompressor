cc889b1443cd9eb0793d306ec4bfb9527084f960
Santiago Báez Duic <93755514+SantiagoBaezDuic@users.noreply.github.com>
2024-11-01 15:16:26 -0300

    First commit

---
+++ src/main/java/huffman/def/BitReader.java
@@ -0,0 +1,15 @@
+package huffman.def;
+
+import java.io.InputStream;
+
+public interface BitReader
+{
+	// establece el inputStream desde donde leerá los bits
+	public void using(InputStream is);
+
+	// retorna 1 o 0, o -1 si es EOF
+	public int readBit();
+	
+	// vacia el buffer
+	public void flush();
+}
---
+++ src/main/java/huffman/def/BitWriter.java
@@ -0,0 +1,15 @@
+package huffman.def;
+
+import java.io.OutputStream;
+
+public interface BitWriter
+{
+	// establece el outputStream donde escribirá los bits
+	public void using(OutputStream os);
+	
+	// escribe un bit
+	public void writeBit(int bit);
+	
+	// completa el buffer con ceros y lo escribe
+	public void flush();
+}
