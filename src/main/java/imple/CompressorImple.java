b370155d80e11c165ddfa719dd17e195ff29a6ad
Santiago Báez Duic <93755514+SantiagoBaezDuic@users.noreply.github.com>
2025-02-08 00:32:52 -0300

    Finished second and third methods from Compressor class

--- src/main/java/imple/CompressorImple.java
+++ src/main/java/imple/CompressorImple.java
@@ -5,6 +5,7 @@ import huffman.util.Console;
 import huffman.def.Compresor;
 import huffman.def.HuffmanInfo;
 import huffman.def.HuffmanTable;
+import huffman.util.HuffmanTree;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -13,6 +14,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CompressorImple implements Compresor {
+
+    int leavesAmount = 0; //Para llevar el conteo de las hojas del árbol
+
     @Override
     // Recorre el archivo y retorna un HuffmanTable[256] contando cuántas veces aparece cada byte
     public HuffmanTable[] contarOcurrencias(String fileName){
@@ -72,20 +76,63 @@ public class CompressorImple implements Compresor {
 
     // Retorna una lista ordenada donde cada nodo representa a cada byte del archivo
     public List<HuffmanInfo> crearListaEnlazada(HuffmanTable arr[]){
+        Console console = Console.get();
+        console.println("Creando lista enlazada...");
+        ArrayList<HuffmanTable> huffmanAList = new ArrayList<>(Arrays.asList(arr)); //ArrayList de partida
         List<HuffmanInfo> huffmanList = new LinkedList<>(); //Lista enlazada que va a ser retornada.
-        ArrayList<HuffmanTable> huffmanAList = new ArrayList<>(Arrays.asList(arr));
 
-        return new LinkedList<>();
+        huffmanAList.sort((e1, e2) -> Integer.compare(e1.getN(), e2.getN())); //Ordeno la lista según cantidad ascendente de ocurrencias
+
+        for (int i = 0; i < 256; i++){
+            HuffmanTable currentNode = huffmanAList.get(i); //Leo el nodo
+
+            if (currentNode.getN() != 0){ //Si el byte tiene alguna ocurrencia
+                leavesAmount++;
+                String code = currentNode.getCod(); //Leo el byte asociado
+                int c = (int)(code.charAt(0)); //Transformo el byte a su expresión en ASCII
+                HuffmanInfo huffmanInfo = new HuffmanInfo(c, currentNode.getN()); //Junto la expresión con la # de ocurrencias
+                huffmanList.add(huffmanInfo); //Añado la hoja/nodo a la lista enlazada
+            }
+        }
+        console.println("Lista enlazada creada con éxito!");
+        return huffmanList;
     };
+
+    private static void orderedInsert(List<HuffmanInfo> lista, HuffmanInfo node){
+        Boolean alreadyInserted = false; //Para saber si ya fue insertado y el proceso debe parar
+        for (int i = 0; i <= lista.size() && !alreadyInserted; i++){
+            if (i == lista.size()){ // Si alcanzamos el final de la lista, inserto el nodo al final
+                lista.add(node);
+                alreadyInserted = true; // Detengo el proceso
+            } else if (node.getN() < lista.get(i).getN()) { //Si la cantidad de ocurrencias es menor que la del nodo siguiente, inserto el nodo
+                lista.add(node);
+                alreadyInserted = true;
+            }
+        }
+    }
 
     // Convierte la lista en el árbol Huffman
     public HuffmanInfo convertirListaEnArbol(List<HuffmanInfo> lista){
-        return new HuffmanInfo();
+        Console c = Console.get();
+        c.println("Convirtiendo lista en árbol...");
+        for (int i=256; lista.size() != 1; i++){
+            HuffmanInfo a = lista.remove(0); //Elimino el primer nodo
+            HuffmanInfo b = lista.remove(0); //Elimino el segundo nodo
+            int newNodeAmount = a.getN() + b.getN(); //Sumo las ocurrencias
+            HuffmanInfo newNode = new HuffmanInfo(i, newNodeAmount); //Creo el nuevo nodo
+            newNode.setRight(a); //Asigno el primer nodo como hijo derecho
+            newNode.setLeft(b); //Asigno el segundo nodo como hijo izquierdo
+            orderedInsert(lista, newNode);
+        }
+        HuffmanInfo rootNode = lista.get(0); //Selecciono el único nodo que quedó en el árbol (el nodo raíz)
+        c.println("Árbol creado con éxito!");
+        return rootNode;
     };
 
     // Recorre el árbol Huffman y completa los códigos en el array
     public void generarCodigosHuffman(HuffmanInfo root,HuffmanTable arr[]){
-
+        HuffmanTree huffmanTree = new HuffmanTree(root);
+        
     };
 
     // Escribe el encabezado en el archivo filename+".huf", y retorna cuántos bytes ocupa el encabezado
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.util.List;
+
 import huffman.def.Compresor;
 import huffman.def.HuffmanTable;
+import huffman.def.HuffmanInfo;
 import huffman.util.Console;
 import imple.Factory;
 
@@ -10,12 +13,16 @@ public class Main {
     static private String fileSelect(){
         Console c = Console.get();
         c.println("Seleccione el archivo a comprimir/descomprimir.");
-        return c.fileExplorer();
+        String selectedFile = c.fileExplorer();
+        c.println("Usted ha seleccionado el archivo: " + selectedFile);
+        return selectedFile;
     }
 
     static private void compressFile(String fileName){
         Compresor compressor = Factory.getCompresor();
         HuffmanTable[] huffmanArray = compressor.contarOcurrencias(fileName);
+        List<HuffmanInfo> huffmanList = compressor.crearListaEnlazada(huffmanArray);
+        HuffmanInfo rootNode = compressor.convertirListaEnArbol(huffmanList);
     }
 
     static private void decompressFile(String fileName){
@@ -24,10 +31,13 @@ public class Main {
 
     public static void main(String[] args) {
         String fileName = fileSelect();
+        Console c = Console.get();
 
         if (fileName.endsWith(".huf")){
+            c.println("Comenzando descompresión");
             decompressFile(fileName);
         } else {
+            c.println("Comenzando compresión");
             compressFile(fileName);
         }
     }
